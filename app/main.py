413efff8aff4b6f28c509ba521c348defa7df39c
Valerii <valerapiliaev@gmail.com>
2025-12-15 21:06:14 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import multiprocessing
 import time
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from hashlib import sha256
-
 
 PASSWORDS_TO_BRUTE_FORCE = [
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
@@ -20,13 +21,49 @@ def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def brute_force_password() -> None:
-    pass
+def find_password(start: int, end: int, hashes_set: set) -> list:
+    result = []
+    for number in range(start, end + 1):
+        password_str = str(number).zfill(8)
+        hashed_number = sha256_hash_str(password_str)
+        if hashed_number in hashes_set:
+            result.append(password_str)
+    return result
+
+
+def brute_force_password() -> dict:
+    total = 100_000_000
+    chunk_size = 100_000
+    hashes_set = set(PASSWORDS_TO_BRUTE_FORCE)
+    futures = []
+    result = {}
+
+    with ProcessPoolExecutor(multiprocessing.cpu_count() - 1) as executor:
+        for start in range(0, total, chunk_size):
+            end = min(start + chunk_size - 1, total - 1)
+            futures.append(
+                executor.submit(find_password, start, end, hashes_set)
+            )
+        for future in as_completed(futures):
+            found_passwords = future.result()
+
+            for password in found_passwords:
+
+                password_hash = sha256_hash_str(password)
+
+                if password_hash not in result:
+                    result[password_hash] = password
+                if len(result) == len(hashes_set):
+                    return result
+
+    return result
 
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    brute_force_password()
+    result = brute_force_password()
     end_time = time.perf_counter()
+    for hashes, passwords in result.items():
+        print(f"{hashes} - {passwords}")
 
     print("Elapsed:", end_time - start_time)
